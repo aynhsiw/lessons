d7f0be9f64581ac377558aeb787efeeb9ba276d2
Vitaliy <okun.vitaliy@gmail.com>
2025-02-03 23:47:14 +0300

    Negative "for" tasks

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,74 +1,78 @@
-﻿Console.WriteLine("Task 1: Sum from 0 to 100\r\n");
+﻿//Console.WriteLine("Task 1: Sum from 0 to 100\r\n");
 
-int sum = 0;
-for (int a = 0; a <= 100; a++)
-{
-    sum += a;
-}
-Console.WriteLine(sum + "\r\n");
-
-
-Console.WriteLine("Task 2: Line-by-line text\r\n");
-for (int stroke = 0; stroke < 100; stroke++)
-{
-    Console.WriteLine("Строка " + stroke);
-}
+//int sum = 0;
+//for (int a = 0; a <= 100; a++)
+//{
+//    sum += a;
+//}
+//Console.WriteLine(sum + "\r\n");
 
 
-Console.WriteLine("Task 3: Numbers 10 to 20 and 40 to 50\r\n");
-
-for (int i = 0; i < 100; i++)
-{
-    if (i >= 10 && i <= 20)
-    {
-        Console.WriteLine(i);
-    }
-
-    else if (i >= 40 && i <= 50)
-    {
-        Console.WriteLine(i);
-    }
-}
+//Console.WriteLine("Task 2: Line-by-line text\r\n");
+//for (int stroke = 0; stroke < 100; stroke++)
+//{
+//    Console.WriteLine("Строка " + stroke);
+//}
 
 
-Console.WriteLine("Task 3-A: Numbers 10 to 20 and 40 to 50\r\n");
+//Console.WriteLine("Task 3: Numbers 10 to 20 and 40 to 50\r\n");
 
-for (int a = 0; a < 100; a++)
-{
-    if (a >= 10 && a <= 20)
-    {
-        Console.WriteLine(a);
-    }
-}
-Console.WriteLine();
-for (int a = 0; a < 100; a++)
-{
-    if (a >= 40 && a <= 50)
-    {
-        Console.WriteLine(a);
-    }
-}
+//for (int i = 0; i < 100; i++)
+//{
+//    if (i >= 10 && i <= 20)
+//    {
+//        Console.WriteLine(i);
+//    }
+
+//    else if (i >= 40 && i <= 50)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
 
 
-Console.WriteLine("Task 4: Last added number\r\n");
-for (int i = 0; i <= 1000; i++)
-{
-    sum += i;
-    if (sum > 100000)
-    {
-        Console.WriteLine("The last added number: " + i);
-        break;
-    }
-}
+//Console.WriteLine("Task 3-A: Numbers 10 to 20 and 40 to 50\r\n");
+
+//for (int a = 0; a < 100; a++)
+//{
+//    if (a >= 10 && a <= 20)
+//    {
+//        Console.WriteLine(a);
+//    }
+//}
+//Console.WriteLine();
+//for (int a = 0; a < 100; a++)
+//{
+//    if (a >= 40 && a <= 50)
+//    {
+//        Console.WriteLine(a);
+//    }
+//}
 
 
-Console.WriteLine("Task 5: Even numbers from 0 to 100\r\n");
+//Console.WriteLine("Task 4: Last added number\r\n");
+//int sum = 0;
+//for (int i = 0; i <= 1000; i++)
+//{
+//    sum += i;
+//    if (sum > 100000)
+//    {
+//        Console.WriteLine("The last added number: " + i);
+//        break;
+//    }
+//}
 
-for (int i = 0; i <= 100; i++)
-{
-    if (i != 0 && i % 2 == 0)
-    {
-        Console.WriteLine(i);
-    }
-}
 
+//Console.WriteLine("Task 5: Even numbers from 0 to 100\r\n");
+
+//for (int i = 0; i <= 100; i += 2)
+//{
+//    Console.WriteLine(i);
+//}
+
+//Console.WriteLine("Task 6: Even negative numbers from 0 to -100\r\n");
+
+//for (int i = 0; i >= -100; i -= 2)
+//{
+//    Console.WriteLine(i);
+//}
